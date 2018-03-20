74eaa37f72f8a70b56016bc38d621e643c83eda1
diana-lucatero <diana.lucatero@gmail.com>
2018-03-20 21:40:40 +0100

    Removal of observed NaN
    First try. Does not work. Issue with object forecast class not being a
    matrix.

--- verify_xin_q.R
+++ verify_xin_q.R
@@ -38,17 +38,18 @@ obs0 <- obs[,2:49] ## Remove dates not modelled
 name_file <- paste0(Dir,'discharge/forecast/sim1/sim_station_',name_st[ist],'.txt')
 sim <- t(matrix(scan(name_file),nrow = 49,ncol = 1085))
 sim1 <- sim[,2:49] ## Remove dates not modelled
-
-
+## Indexes of missing observed values (NaN) for removal
+ind0 <- unique(which(is.na(obs0)==TRUE,arr.ind = TRUE)[,1])
+num.ranks <- 1086 - length(ind0)
 ## ---- Verification ------ ##
 verA <- array(NaN,dim = c(48,4))
 verB <- array(NaN,dim = c(48,4))
-rank.B <- array(NaN,dim = c(1086,48))
+rank.B <- array(NaN,dim = c(num.ranks,48))
 verC <- array(NaN,dim = c(48,4))
 verD <- array(NaN,dim = c(48,4))
-rank.D <- array(NaN,dim = c(1086,48))
+rank.D <- array(NaN,dim = c(num.ranks,48))
 verE <- array(NaN,dim = c(48,4))
-rank.E <- array(NaN,dim = c(1086,48))
+rank.E <- array(NaN,dim = c(num.ranks,48))
 colnames0 <- c('bias','rmse','crps','alpha')
 colnames(verB)<-colnames0
 colnames(verC)<-colnames0
@@ -62,10 +63,10 @@ for (ilead in 1:48){#48
   forecast <- matrix(sim3[,ilead],nrow = 1086,ncol = no_ens,byrow = TRUE)
   forecast_mean <- rowMedians(forecast)
   ## Bias [forecast-observation]
-  verE[ilead,1] <- mean(forecast_mean,na.rm = T) - mean(obs0[,ilead],na.rm = T)
-  verE[ilead,2] <- rmse(forecast_mean,obs0[,ilead],na.rm=T)
-  verE[ilead,3] <- mean(EnsCrps(forecast,obs0[,ilead]),na.rm = TRUE)
-  ranks <- apply(cbind(obs0[,ilead],forecast), 1, rank, ties.method="last")[1, ]
+  verE[ilead,1] <- mean(forecast_mean[-ind0],na.rm = T) - mean(obs0[-ind0,ilead],na.rm = T)
+  verE[ilead,2] <- rmse(forecast_mean[-ind0],obs0[-ind0,ilead],na.rm=T)
+  verE[ilead,3] <- mean(EnsCrps(forecast[-ind0,],obs0[-ind0,ilead]),na.rm = TRUE)
+  ranks <- apply(cbind(obs0[-ind0,ilead],forecast[-ind0,]), 1, rank, ties.method="last")[1, ]
   rank_m <- (ranks-1)/no_ens
   f <- ecdf(rank_m)
   verE[ilead,4] <- 1-2*mean(abs(f(probs0)-probs0))
@@ -74,10 +75,10 @@ for (ilead in 1:48){#48
   forecast <- matrix(sim2[,ilead],nrow = 1086,ncol = no_ens,byrow = TRUE)
   forecast_mean <- rowMedians(forecast)
   ## Bias [forecast-observation]
-  verB[ilead,1] <- mean(forecast_mean,na.rm = T) - mean(obs0[,ilead],na.rm = T)
-  verB[ilead,2] <- rmse(forecast_mean,obs0[,ilead],na.rm=T)
-  verB[ilead,3] <- mean(EnsCrps(forecast,obs0[,ilead]),na.rm = TRUE)
-  ranks <- apply(cbind(obs0[,ilead],forecast), 1, rank, ties.method="last")[1, ]
+  verB[ilead,1] <- mean(forecast_mean[-ind0],na.rm = T) - mean(obs0[-ind0,ilead],na.rm = T)
+  verB[ilead,2] <- rmse(forecast_mean[-ind0],obs0[-ind0,ilead],na.rm=T)
+  verB[ilead,3] <- mean(EnsCrps(forecast[-ind0,],obs0[-ind0,ilead]),na.rm = TRUE)
+  ranks <- apply(cbind(obs0[-ind0,ilead],forecast[-ind0,]), 1, rank, ties.method="last")[1, ]
   rank_m <- (ranks-1)/no_ens
   f <- ecdf(rank_m)
   verB[ilead,4] <- 1-2*mean(abs(f(probs0)-probs0))
@@ -86,10 +87,10 @@ for (ilead in 1:48){#48
   forecast <- matrix(sim4[,ilead],nrow = 1086,ncol = no_ens,byrow = TRUE)
   forecast_mean <- rowMedians(forecast)
   ## Bias [forecast-observation]
-  verD[ilead,1] <- mean(forecast_mean,na.rm = T) - mean(obs0[,ilead],na.rm = T)
-  verD[ilead,2] <- rmse(forecast_mean,obs0[,ilead],na.rm=T)
-  verD[ilead,3] <- mean(EnsCrps(forecast,obs0[,ilead]),na.rm = TRUE)
-  ranks <- apply(cbind(obs0[,ilead],forecast), 1, rank, ties.method="last")[1, ]
+  verD[ilead,1] <- mean(forecast_mean[-ind0],na.rm = T) - mean(obs0[-ind0,ilead],na.rm = T)
+  verD[ilead,2] <- rmse(forecast_mean[-ind0],obs0[-ind0,ilead],na.rm=T)
+  verD[ilead,3] <- mean(EnsCrps(forecast[-ind0,],obs0[-ind0,ilead]),na.rm = TRUE)
+  ranks <- apply(cbind(obs0[-ind0,ilead],forecast[-ind0,]), 1, rank, ties.method="last")[1, ]
   rank_m <- (ranks-1)/no_ens
   f <- ecdf(rank_m)
   verD[ilead,4] <- 1-2*mean(abs(f(probs0)-probs0))
@@ -98,16 +99,16 @@ for (ilead in 1:48){#48
   forecast <- matrix(sim5[,ilead],nrow = 1086,ncol = 1,byrow = TRUE)
   forecast_mean <- rowMedians(forecast)
   ## Bias [forecast-observation]
-  verC[ilead,1] <- mean(forecast_mean,na.rm = T) - mean(obs0[,ilead],na.rm = T)
-  verC[ilead,2] <- rmse(forecast_mean,obs0[,ilead],na.rm=T)
-  verC[ilead,3] <- mean(EnsCrps(forecast,obs0[,ilead]),na.rm = TRUE)
+  verC[ilead,1] <- mean(forecast_mean[-ind0],na.rm = T) - mean(obs0[-ind0,ilead],na.rm = T)
+  verC[ilead,2] <- rmse(forecast_mean[-ind0],obs0[-ind0,ilead],na.rm=T)
+  verC[ilead,3] <- mean(EnsCrps(forecast,obs0[-ind0,ilead]),na.rm = TRUE)
   ## No DA - No perturbed -- sim A
   forecast <- matrix(sim1[,ilead],nrow = 1085,ncol = 1,byrow = TRUE)
   forecast_mean <- rowMedians(forecast)
   ## Bias [forecast-observation]
-  verA[ilead,1] <- mean(forecast_mean,na.rm = T) - mean(obs0[c(1:1085),ilead],na.rm = T)
-  verA[ilead,2] <- rmse(forecast_mean,obs0[c(1:1085),ilead],na.rm=T)
-  verA[ilead,3] <- mean(EnsCrps(forecast,obs0[c(1:1085),ilead]),na.rm = TRUE)
+  verA[ilead,1] <- mean(forecast_mean[-ind0],na.rm = T) - mean(obs0[-ind0,ilead],na.rm = T)
+  verA[ilead,2] <- rmse(forecast_mean[-ind0],obs0[-ind0,ilead],na.rm=T)
+  verA[ilead,3] <- mean(EnsCrps(forecast[-ind0,],obs0[-ind0,ilead]),na.rm = TRUE)
 }# End lead time
 
 file0 <- paste0(Dir,'xin_data/discharge/performance_B.txt')
